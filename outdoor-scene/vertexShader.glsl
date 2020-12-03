47c06941e7e2d745e3d8a52affda1e0a94e1fc85
WONG ZHENG QUAN <38060298+wongzhengquan@users.noreply.github.com>
2020-12-03 11:38:57 +0800

    Added vertexShader.glsl code

---
+++ outdoor-scene/vertexShader.glsl
@@ -0,0 +1,31 @@
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoord;
+layout (location = 3) in vec3 bPos;
+layout (location = 4) in vec3 bNormal;
+layout (location = 5) in vec2 bTexCoord;
+
+out vec3 vNormal;
+out vec3 vPos;
+out vec2 vTexCoord;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 proj;
+uniform int obj;
+
+void main() {
+	if (obj == 1) {
+		gl_Position = proj * view * model * vec4(aPos, 1.0);
+		vPos = vec3(model * vec4(aPos, 1.0));
+		vNormal = vec3(model * vec4(aNormal, 0.0));
+		vTexCoord = aTexCoord;
+	}
+	else {
+		gl_Position = proj * view * model * vec4(bPos, 1.0);
+		vPos = vec3(model * vec4(bPos, 1.0));
+		vNormal = vec3(model * vec4(bNormal, 0.0));
+		vTexCoord = bTexCoord;
+	}
+}
